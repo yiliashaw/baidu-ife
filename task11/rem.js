64c817140a78599249c4c5399ec1780f89e640ac
yiliashaw <yilia.shaw@gmail.com>
2016-08-29 14:23:21 +0800

    scale

--- task11/rem.js
+++ task11/rem.js
@@ -1,10 +1,16 @@
 function init() {
   var width = window.innerWidth;
   var dpr = devicePixelRatio;
+  var meta = document.querySelector('meta[name="viewport"]');
+  var scale = 1/dpr;
+  meta.setAttribute('content', 'initial-scale=' + scale);
   if (width/dpr > 540) {
     width = 540 * dpr;
   }
   document.documentElement.style.fontSize = (width / 100) + 'px';
+
+
 }
+
 init();
 window.onresize = init;
