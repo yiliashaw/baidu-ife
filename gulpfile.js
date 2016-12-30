d1102c908d74860c04dbf68a8a4048a3c024cb01
opengg <liy099@gmail.com>
2016-12-30 14:30:07 +0800

    eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 (() => {
-  'use strict';
+  const eslint = require('gulp-eslint');
 
   const gulp = require('gulp');
   const stylus = require('gulp-stylus');
   const pug = require('gulp-pug');
+
+  const gulpIf = require('gulp-if');
   // const rename = require('gulp-rename');
   // const debug = require('gulp-debug');
 
@@ -11,18 +13,21 @@
 
   const plumber = require('gulp-plumber');
 
-  const { log, colors } = require('gulp-util');
+  const {
+    log,
+    colors
+  } = require('gulp-util');
 
   const plumberfy = () =>
     plumber({
-      errorHandler: function (error) {
+      errorHandler(error) {
         log(
           colors.cyan('Plumber') + colors.red(' found unhandled error:\n'),
           error.toString()
         );
         this.emit('end');
       }
-    })
+    });
 
   const path = require('path');
 
@@ -33,29 +38,66 @@
 
   const taskFactory = (path, ext, transform) =>
     () =>
-      gulp.src(path)
-        .pipe(plumberfy())
-        .pipe(transform())
-        // .pipe(debug(ext))
-        // .pipe(rename(
-        //   path => {
-        //     path.extname = ext;
-        //   }
-        // ))
-        .pipe(gulp.dest(file => file.base))
+    gulp.src(path)
+    .pipe(plumberfy())
+    .pipe(transform())
+    // .pipe(debug(ext))
+    // .pipe(rename(
+    //   path => {
+    //     path.extname = ext;
+    //   }
+    // ))
+    .pipe(gulp.dest(file => file.base));
 
   const taskStyl = taskFactory(stylFiles, '.css', stylus);
+
   const taskPug = taskFactory(pugFiles, '.html', pug);
 
   gulp.task('styl', taskStyl);
+
   gulp.task('pug', taskPug);
 
-  gulp.task('watch', ['styl', 'pug'], cb => {
-    watch(stylFiles, { cwd: './', verbose: true }, taskStyl);
-    watch(pugFiles, { cwd: './', verbose: true }, taskPug);
+  gulp.task('watch', ['styl', 'pug'], (cb) => {
+    watch(stylFiles, {
+      cwd: './',
+      verbose: true
+    }, taskStyl);
+
+    watch(pugFiles, {
+      cwd: './',
+      verbose: true
+    }, taskPug);
+
     setTimeout(cb, 1);
   });
 
   gulp.task('default', ['styl', 'pug'], cb => setTimeout(cb, 1));
 
+  // Has ESLint fixed the file contents?
+  const isFixed = ({
+    eslint
+  }) => eslint !== null && eslint.fixed;
+
+  gulp.task('lint-fix', () =>
+    gulp.src([
+      './**/*.js',
+      '!node_modules/**/*'
+    ])
+    .pipe(eslint({
+      fix: true
+    }))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+    .pipe(gulpIf(isFixed, gulp.dest('./')))
+  );
+
+  gulp.task('lint', () =>
+    gulp.src([
+      './**/*.js',
+      '!node_modules/**/*'
+    ])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+  );
 })();
